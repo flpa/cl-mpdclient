06dbe436a27dfb0bfdb83946fb55a7197cf3ad60
flpa <flpa.dev@zoho.com>
2015-05-12 13:45:20 +0200

    mpd api tests

--- cl-mpdclient.lisp
+++ cl-mpdclient.lisp
@@ -3,24 +3,44 @@
 (in-package #:cl-mpdclient)
 
 (defparameter *conn* (connect))
+(disconnect *conn*)
 
-;; caused error related to keyword :albumartist?
 (now-playing *conn*)
-(play *conn*)
-(pause *conn*)
+;;(play *conn*)
+;;(pause *conn*)
+(status *conn*)
 
-(let ((scr (initscr))
-      (win1 (newwin 18 13 4 19)))
-  (nodelay scr FALSE)
-  (erase)
-  (cbreak)
-  (noecho)
-  (mvwaddstr scr 23 2 "Type any thing to exit.")
-  (getch)
-  (wrefresh scr)
-  (delwin win1)
-  (endwin)
-  )
+(ping *conn*)
+
+;; returns list of strings like ("Amon Amarth" "Wither") ...
+(list-metadata *conn* 'artist)
+;;returns niL?
+(mpd::parse-list (list-metadata *conn* 'artist))
+
+(list-metadata *conn* 'genre)
+(playlist-info *conn*)
+(tag-types *conn*)
+(search-tracks *conn* :artist "Amon Amarth")
 
 
-
+(defun curse-test () 
+  (let ((scr (initscr))
+        (win1 (newwin 10 10 10 10)))
+    (nodelay scr TRUE)
+    (erase)
+    (cbreak)
+    (noecho)
+    (mvwaddstr scr 10 2 "Type any thing to exit.")
+    (mvwaddstr win1 0 0 "The window.")
+    (wrefresh scr)
+    (wrefresh win1)
+    (loop for input = (getch) 
+          while (not (eql (char-code #\q) input))
+          do (sb-unix:nanosleep 0 100)
+          if (not (eql ERR input))
+          do (format t "'~a'~%" input))
+    (delwin win1)
+    (delwin scr)
+    (endwin)
+    (refresh)
+    ))
