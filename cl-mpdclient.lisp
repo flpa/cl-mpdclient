2effa194c430795195e17137f40b07e460743a6d
flpa <flpa.dev@zoho.com>
2015-05-12 15:11:26 +0200

    print artists in single screen

--- cl-mpdclient.lisp
+++ cl-mpdclient.lisp
@@ -2,6 +2,11 @@
 
 (in-package #:cl-mpdclient)
 
+(defun firstn (list n)
+  (if (< (length list) n)
+    list
+    (butlast list (- (length list) n))))
+
 (defparameter *conn* (connect))
 (disconnect *conn*)
 
@@ -13,7 +18,10 @@
 (ping *conn*)
 
 ;; returns list of strings like ("Amon Amarth" "Wither") ...
+
+
 (list-metadata *conn* 'artist)
+(length (list-metadata *conn* 'artist))
 ;;returns niL?
 (mpd::parse-list (list-metadata *conn* 'artist))
 
@@ -22,10 +30,31 @@
 (tag-types *conn*)
 (search-tracks *conn* :artist "Amon Amarth")
 
+(defparameter *max-artists* 40)
+
+(defun floclient () 
+  (let ((scr (initscr))
+        (mpdconn (connect)))
+    (nodelay scr TRUE)
+    (cbreak)
+    (noecho)
+    (loop for artist in (firstn (list-metadata mpdconn 'artist) *max-artists*)
+          for i from 10
+          do 
+          (mvwaddstr scr i 2 artist))
+    (loop for input = (getch) 
+          while (not (eql (char-code #\q) input))
+          do (sb-unix:nanosleep 0 100)
+          if (not (eql ERR input))
+          do (format t "'~a'~%" input))
+    (wrefresh scr)
+    (endwin)
+    (disconnect mpdconn)
+    ))
 
 (defun curse-test () 
   (let ((scr (initscr))
-        (win1 (newwin 10 10 10 10)))
+        (win1 (newwin 20 20 20 20)))
     (nodelay scr TRUE)
     (erase)
     (cbreak)
@@ -34,13 +63,7 @@
     (mvwaddstr win1 0 0 "The window.")
     (wrefresh scr)
     (wrefresh win1)
-    (loop for input = (getch) 
-          while (not (eql (char-code #\q) input))
-          do (sb-unix:nanosleep 0 100)
-          if (not (eql ERR input))
-          do (format t "'~a'~%" input))
     (delwin win1)
     (delwin scr)
     (endwin)
-    (refresh)
     ))
