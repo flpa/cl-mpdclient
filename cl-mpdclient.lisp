75b1f07d7da8a0e380d8edeb5381aa6ff9ab51c9
flpa <flpa.dev@zoho.com>
2015-06-27 22:48:14 +0200

    'search' progress

--- cl-mpdclient.lisp
+++ cl-mpdclient.lisp
@@ -16,6 +16,10 @@
   (sb-ext:disable-debugger)
   (floclient))
 
+(defun fetch-artists (mpdconn)
+  (mapcar #'(lambda (x) (subseq x 8)) 
+          (list-metadata mpdconn 'artist)))
+
 (defun floclient () 
   (initscr)
   (let* ((mpdconn (connect))
@@ -24,9 +28,10 @@
          (lines (1- *LINES*))
          (artists-width (floor (/ *COLS* 3)))
          (stdout nil)
-         (artists (mapcar #'(lambda (x) (subseq x 8)) 
-                          (list-metadata mpdconn 'artist)))
-         (pad (newpad (length artists) 30)))
+         (artists (fetch-artists mpdconn))
+         (pad (newpad (length artists) 30))
+         (searching nil)
+         (term (make-array 5 :fill-pointer 0 :adjustable t :element-type 'character)))
     (nodelay pad (if *async* TRUE FALSE))
     (halfdelay 10)
     (cl-ncurses:clear)
@@ -40,57 +45,57 @@
     (refresh)
     (loop for input = (getch) 
           for status = (status mpdconn)
-          for selected = (nth cursor-line artists)
-          while (not (eql (char-code #\q) input))
           do (progn
+               ;;print duration
                (mvprintw 3 50 (format nil "~a/~a" (first (duration status))
                                       (second (duration status))))
-               ;;move cursor back to current position
-               (cl-ncurses:move (1+ cursor-line) 0)  
+               ;;print term
+               (mvprintw 4 50 (format nil "~a" term))
                (unless (eql ERR input)
                  (prefresh pad scroll-index 0 1 0 lines artists-width)
                  (when *async* (sb-unix:nanosleep 0 100))
-                 (case (code-char input)
-                   ;;limit on lower bound
-                   (#\j (if (= cursor-line lines)       
-                          (incf scroll-index)
-                          (cl-ncurses:move (1+ (incf cursor-line)) 0)))
-                   (#\k (if (zerop cursor-line)       
-                          (decf scroll-index)
-                          (cl-ncurses:move (1+ (decf cursor-line)) 0)))
-                   (#\P (pause mpdconn))
-                   (#\t
-                    (progn 
-                      (setf scroll-index 0)
-                      (prefresh pad scroll-index 0 1 0 lines artists-width)
-                      (cl-ncurses:move (setf cursor-line 1) 0))  
-                    )
-                   (#\c (mpd:clear mpdconn))
-                   (#\G (progn ;;TODO
-                          (cl-ncurses:move (setf cursor-line (length artists)) 0)
-                          ;;(setf scroll-index *max-artists*)
-                          ))
-                   (#\Newline 
-                    (progn
-                      (format stdout "Selected ~a~%" selected)
-                      (mpd:clear mpdconn)
-                      ;;works for some
-                      ;;(add mpdconn selected) 
-                      (add mpdconn (first (find-tracks mpdconn :artist selected)))
-                      (play mpdconn)))
-                   (#\s (progn 
-                          (format stdout "Type to search...~%")
-                          (loop for c = (code-char (getch))
-                                ;; resizable string
-                                with term = (make-array 5 :fill-pointer 0 :adjustable t :element-type 'character)
-                                while (not (eql #\q c))
-                                ;; escape on \Esc
-                                do (progn
-                                     (vector-push-extend c term)
-                                     (format stdout "Matching ~a~%" term)
-                                     (format stdout "~a~%" (matching-artists artists term))))
-                          (format stdout "End of search~%")))
-                   (otherwise (format stdout "'~a'~%" input))))))
+                 ;;if in search: append char to search term
+                 (if searching
+                   (case (code-char input)
+                     (#\Esc
+                      (progn
+                        (setf searching nil)
+                        (loop while (> (length term) 0) do (vector-pop term))))
+                     (#\Backspace (if (> (length term) 0) 
+                                    (vector-pop term)
+                                    (setf searching nil)))
+                     (otherwise (vector-push-extend (code-char input) term)))
+                   (case (code-char input)
+                     (#\q (return))
+                     ;;limit on lower bound
+                     (#\j (if (= cursor-line lines)       
+                            (incf scroll-index)
+                            (cl-ncurses:move (1+ (incf cursor-line)) 0)))
+                     (#\k (if (zerop cursor-line)       
+                            (decf scroll-index)
+                            (cl-ncurses:move (1+ (decf cursor-line)) 0)))
+                     (#\P (pause mpdconn))
+                     (#\t
+                      (progn 
+                        (setf scroll-index 0)
+                        (prefresh pad scroll-index 0 1 0 lines artists-width)
+                        (cl-ncurses:move (setf cursor-line 1) 0))  
+                      )
+                     (#\c (mpd:clear mpdconn))
+                     (#\G (progn ;;TODO
+                            (cl-ncurses:move (setf cursor-line (length artists)) 0)
+                            ;;(setf scroll-index *max-artists*)
+                            ))
+                     (#\Newline 
+                      (let ((selected (nth cursor-line artists)))
+                        (format stdout "Selected ~a~%" selected)
+                        (mpd:clear mpdconn)
+                        ;;works for some
+                        ;;(add mpdconn selected) 
+                        (add mpdconn (first (find-tracks mpdconn :artist selected)))
+                        (play mpdconn)))
+                     (#\s (setf searching t))
+                     (otherwise (format t "'~a'~%" input)))))))
     (delwin pad)
     (disconnect mpdconn))
   (endwin))
